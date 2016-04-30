263af53948dbd5e1332ad535a5bb643c179d79ef
Saeid Abolfazli <abolfazli.s@gmail.com>
2016-05-01 01:12:03 +0800

    Added new Q&A

--- main.R
+++ main.R
@@ -52,13 +52,40 @@ CPS <- merge(CPS,MetroAreaMap, by.x = "MetroAreaCode", by.y="Code", all.x = TRUE
 CPS<- merge(CPS,CountryCodes, by.x = "CountryOfBirthCode", by.y="Code", all.x = TRUE)
 names(CPS)
 
-Which of the following metropolitan areas has the largest number of interviewees?
-
+#Which of the following metropolitan areas has the largest number of interviewees?
 table(CPS$MetroArea)
 
 #How many interviewees have a missing value for the new country of birth variable?
 table(is.na(CPS$Country))
 
+#Which metropolitan area has the highest proportion of interviewees of Hispanic ethnicity? Hint: Use tapply() with mean, as in the previous subproblem. Calling sort() on the output of tapply() could also be helpful here.
+sort(tapply(CPS$Hispanic==1,CPS$MetroArea,mean))
 
-Among all interviewees born outside of North America, which country was the most common place of birth?
-table(CPS$Country)
+#determine the number of metropolitan areas in the United States from which at least 20% of interviewees are Asian.
+sort(tapply(CPS$Race=="Asian",CPS$MetroArea,mean))
+
+
+#which metropolitan area has the smallest proportion of interviewees who have received no high school diploma
+sort(tapply(CPS$Education == "No high school diploma", CPS$MetroArea, mean, na.rm=TRUE))
+
+#Among all interviewees born outside of North America, which country was the most common place of birth?
+NorthAmerica <- c(  "United States","Canada","Mexico","Guatemala","Cuba","Haiti","Dominican Republic","Guadeloupe","Martinique",
+                    "Honduras","Nicaragua","El Salvador","Costa Rica","Panama","Puerto Rico","Jamaica","Trinidad and Tobago",
+                    "Montserrat","Bahamas","Belize","Barbados","Saint Lucia","St. Lucia","Curaçao","Aruba","St. Vincent and the Grenadines",
+                    "United States Virgin Islands","Grenada","Antigua and Barbuda","Dominica","Bermuda","Cayman Islands","Greenland",
+                    "St. Kitts--Nevis","Sint Maarten","Turks and Caicos Islands","Saint Martin","British Virgin Islands","Caribbean Netherlands",
+                    "Anguilla","Saint Barthélemy","Saint Pierre and Miquelon","Montserrat")
+
+Non_NA <- CPS[!CPS$Country %in% NorthAmerica,]
+sort(table(Non_NA$Country))
+
+#What proportion of the interviewees from the "New York-Northern New Jersey-Long Island, NY-NJ-PA" metropolitan area have a country of
+#birth that is not the United States? For this computation, don't include people from this metropolitan area who have a missing country of birth.
+NY_NJ_PA <- CPS[CPS$MetroArea=="New York-Northern New Jersey-Long Island, NY-NJ-PA",]
+NY_NJ_PA$Non_US <- ifelse(NY_NJ_PA$Country=="United States",FALSE, TRUE)
+prop.table(table(NY_NJ_PA$Non_US))
+
+#Which metropolitan area has the largest number (note -- not proportion) of interviewees with a country of birth in India?
+sort(tapply(CPS$Country=="India",CPS$MetroArea,sum,na.rm=TRUE))
+sort(tapply(CPS$Country=="Brazil",CPS$MetroArea,sum,na.rm=TRUE))
+sort(tapply(CPS$Country=="Somalia",CPS$MetroArea,sum,na.rm=TRUE))
